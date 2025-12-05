9d8e25914d4b1a1cbbfec144b5af66876fd335e9
FRCRoboManiacs8140 <mc2stemfablab@outlook.com>
2025-12-05 15:17:59 -0500

    PP is working but graphs are innacurate due to code issue see line 86 of DriveSubsystem to fix it.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,9 +87,9 @@ public final class Constants {
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
-    public static final double kPXController = 0;
+    public static final double kPXController = 6;
     public static final double kPYController = 0;
-    public static final double kPThetaController = 0.15;
+    public static final double kPThetaController = 2;
 
     // Constraint for the motion profiled robot angle controller
     public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -82,11 +82,17 @@ public class DriveSubsystem extends SubsystemBase {
        this::getPose, // Robot pose supplier
        this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-       (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+       (speeds, feedforwards) -> driveRobotRelative(
+        new ChassisSpeeds(
+            speeds.vxMetersPerSecond,
+            speeds.vyMetersPerSecond,
+            -speeds.omegaRadiansPerSecond // Invert the rotational velocity
+        )
+    ), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
        
        new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
-               new PIDConstants(AutoConstants.kPXController, AutoConstants.kPYController, 0.0), // Translation PID constants
-               new PIDConstants(0, 0.0, AutoConstants.kPThetaController) // Rotation PID constants
+               new PIDConstants(AutoConstants.kPXController, 0, 0.0), // Translation PID constants
+               new PIDConstants(AutoConstants.kPThetaController, 0, 0) // Rotation PID constants
        ),
        config, // The robot configuration
        () -> {
